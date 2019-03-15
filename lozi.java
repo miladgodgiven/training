5d4e0e4712f34f9c943cf7cd59de554eb4511676
milad <48551824+miladgodgiven@users.noreply.github.com>
2019-03-15 16:48:27 +0330

    Add files via upload

---
+++ lozi.java
@@ -0,0 +1,17 @@
+//milad.khodadadi
+package lozi;
+
+public class lozi {
+
+	public static void main(String[] args) {
+		
+		System.out.println("   *   ");
+		System.out.println("  ***  ");
+		System.out.println(" ***** ");
+		System.out.println("*******");
+		System.out.println(" ***** ");
+		System.out.println("  ***  ");
+		System.out.println("   *   ");
+	}
+
+}
